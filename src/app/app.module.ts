fc284cedfe37432bc8b6b33e06a64ca63a7303c6
Casper Koksby Møller Nielsen <casp8660@zbc.dk>
2020-08-11 10:41:05 +0200

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TweetsComponent } from './tweets/tweets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TweetsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
